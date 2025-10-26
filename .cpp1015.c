5d97c719492e0ae31be5cae7ccd2e15fd7c61e1b
maimunaibnat581 <munayuqisong@gmail.com>
2025-10-26 19:46:44 +0600

    Add files via upload

---
+++ .cpp1015.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    int maiorAB = (A + B + abs(A - B)) / 2;
+    int maior = (maiorAB + C + abs(maiorAB - C)) / 2;
+    cout << maior << " eh o maior" << endl;
+    return 0;
+}
