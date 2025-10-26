5f04852cf4769188f9e5c446ae7ef800de16725a
maimunaibnat581 <munayuqisong@gmail.com>
2025-10-26 21:11:53 +0600

    Add files via upload

---
+++ .cpp1043.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(1);
+    if (a < b + c && b < a + c && c < a + b)
+        cout << "Perimetro = " << (a + b + c) << endl;
+    else
+        cout << "Area = " << ((a + b) * c / 2.0) << endl;
+}
